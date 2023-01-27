c3fc48283c5fffa09aa89e85a37e58ee281a0aeb
AnikHaque <ekramul15-811@diu.edu.bd>
2023-01-27 17:12:24 +0600

    console cleared

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,7 +9,7 @@ const Home = () => {
   return (
     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10'>
      {
-      data.map(product=><ProductCard product={product} />)
+      data.map(product=><ProductCard key={product.model} product={product} />)
      }
     </div>
   );
