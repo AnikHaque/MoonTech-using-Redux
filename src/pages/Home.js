44c07f695ccd33e4402a86975ef7c925d03b23de
AnikHaque <ekramul15-811@diu.edu.bd>
2023-01-27 17:08:54 +0600

    data fetched using normal fetch

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
+import ProductCard from "../components/ProductCard";
 
 const Home = () => {
+  const [data, setData] = useState([]);
+  fetch('products.json')
+  .then(res=>res.json())
+  .then(data=>setData(data))
   return (
     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10'>
-      <h1>This is home page</h1>
+     {
+      data.map(product=><ProductCard product={product} />)
+     }
     </div>
   );
 };
