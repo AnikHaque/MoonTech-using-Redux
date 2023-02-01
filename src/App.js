f0caa0a6736efdaa8ab0a106e23053f717f61350
AnikHaque <ekramul15-811@diu.edu.bd>
2023-02-01 23:06:40 +0600

    setting up a store

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { Provider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
+import store from "./redux/store";
 import routes from "./routes/routes";
 
 function App() {
   return (
     <div>
+      <Provider store={store}>
       <RouterProvider router={routes} />
+  </Provider>
+      
     </div>
   );
 }
